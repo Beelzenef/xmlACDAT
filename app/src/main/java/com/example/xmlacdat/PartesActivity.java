8d9d28ae6447804b369028360b022efe9a774b88
Elena G <elena.guzbla@gmail.com>
2017-12-12 12:47:12 +0100

    leyendo xml simple

--- app/src/main/java/com/example/xmlacdat/PartesActivity.java
+++ app/src/main/java/com/example/xmlacdat/PartesActivity.java
@@ -2,12 +2,36 @@ package com.example.xmlacdat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.xmlacdat.utils.CheckXML;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 
 public class PartesActivity extends AppCompatActivity {
+
+    private TextView txtV_partesXML;
+
+    public  static final String TEXTO = "<texto><uno>Hello World! </uno><dos>Goodbye</dos></texto>";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_partes);
+
+        txtV_partesXML = (TextView) findViewById(R.id.txtV_PartesXML);
+
+        leerXML();
+    }
+
+    private void leerXML() {
+        try {
+            txtV_partesXML.setText(CheckXML.analizar(TEXTO));
+        } catch (IOException | XmlPullParserException e)
+        {
+            txtV_partesXML.setText(e.getMessage());
+        }
     }
 }
---
+++ app/src/main/java/com/example/xmlacdat/utils/CheckXML.java
@@ -0,0 +1,40 @@
+package com.example.xmlacdat.utils;
+
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ * Analizador de XML
+ */
+
+public class CheckXML {
+
+    public static String analizar(String texto) throws XmlPullParserException, IOException {
+        StringBuilder cadena = new StringBuilder();
+        XmlPullParser xpp = Xml.newPullParser();
+        xpp.setInput(new StringReader(texto));
+        int eventType = xpp.getEventType();
+        cadena.append("Leyendo XML:\n " );
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+
+            if (eventType == XmlPullParser.START_DOCUMENT)
+                cadena.append("START DOCUMENT \n");
+            if (eventType == XmlPullParser.START_TAG)
+                cadena.append("START TAG: " + xpp.getName() + "\n");
+            if (eventType == XmlPullParser.TEXT)
+                cadena.append("CONTENT: " + xpp.getText() + "\n");
+            if (eventType == XmlPullParser.END_TAG)
+                cadena.append("END TAG: " + xpp.getName() + "\n");
+            eventType = xpp.next();
+        }
+
+        cadena.append("END DOCUMENT");
+        return cadena.toString();
+    }
+
+}
